b2af6b3764cbf01a3e9e6c0c01b4bac9a8a249e2
dikxarper <diasmerekeev1@gmail.com>
2022-07-22 16:46:20 +0600

    I

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.set("layout", "layouts/layout")
 app.use(expressLayouts)
 app.use(express.static("public"))
 
-//connecting to mongoDB
+//connecting to mongoDB jj
 const mongoose = require("mongoose")
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
 const db = mongoose.connection
