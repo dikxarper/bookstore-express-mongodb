6a81c5bd7e35a9dbc5f9cd9a8ab423d1e813571a
dikxarper <diasmerekeev1@gmail.com>
2022-07-22 16:00:37 +0600

    Feeling good

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require("express")
 const app = express()
+const expressLayouts = require("express-ejs-layouts")
+const dotenv = require("dotenv").config
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.set("view engine", "ejs")
+app.set("views", __dirname + "/views")
+app.set("layout", "layouts/layout")
+
+app.use(expressLayouts)
+app.use(express.static("public"))
+
+//connecting mongoDB
+const mongoose = require("mongoose")
+mongoose.connect(process.env.DATABASE_URL)
 
 app.get("/", (req, res) => {
   res.render("index")
